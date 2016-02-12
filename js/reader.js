c2a4b81386069051b65c26b4fb8459b474c4b894
Sig Tzeng <sig.tzeng@gmail.com>
2016-02-12 13:10:06 +0800

    readevalloop

--- js/reader.js
+++ js/reader.js
@@ -15,6 +15,7 @@ Reader.prototype.next = function() { return this.tokens[this.position++]; }
 Reader.prototype.peek = function() { return this.tokens[this.position]; }
 
 function tokenize(str) {
+    console.log(str);
     var re = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)/g;
     var results = [];
     while ((match = re.exec(str)[1]) != '') {
