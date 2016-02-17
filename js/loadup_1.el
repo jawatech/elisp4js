4bb4c7ddbc9b945fca1dad76461710b68a8ca187
Sig Tzeng <sig.tzeng@gmail.com>
2016-02-17 18:25:41 +0800

    README.html

---
+++ js/loadup_1.el
@@ -0,0 +1,17 @@
+﻿(if (or (equal (nth 3 command-line-args) "bootstrap")
+	(equal (nth 4 command-line-args) "bootstrap")
+	;; FIXME this is irritatingly fragile.
+	(equal (nth 4 command-line-args) "unidata-gen.el")
+	(equal (nth 7 command-line-args) "unidata-gen-files")
+	(if (fboundp 'dump-emacs)
+	    (string-match "src/bootstrap-emacs" (nth 0 command-line-args))
+	  t))
+    (let ((dir (car load-path)))
+      ;; We'll probably overflow the pure space.
+      (setq purify-flag nil)
+      (setq load-path (list (expand-file-name "." dir)
+			    (expand-file-name "emacs-lisp" dir)
+			    (expand-file-name "language" dir)
+			    (expand-file-name "international" dir)
+			    (expand-file-name "textmodes" dir)
+			    (expand-file-name "vc" dir)))))
